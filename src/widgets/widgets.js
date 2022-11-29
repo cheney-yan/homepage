c3c127a476cec66c4e4fce5a0364322ebf3777b8
cheney yan <cheney@sonder.io>
2022-11-30 00:00:03 +1100

    remove

--- src/widgets/widgets.js
+++ src/widgets/widgets.js
@@ -10,7 +10,6 @@ import gluetun from "./gluetun/widget";
 import gotify from "./gotify/widget";
 import hdhomerun from "./hdhomerun/widget";
 import homebridge from "./homebridge/widget";
-import ipsonder from "./ipsonder/widget";
 import jackett from "./jackett/widget";
 import jellyseerr from "./jellyseerr/widget";
 import lidarr from "./lidarr/widget";
@@ -57,7 +56,6 @@ const widgets = {
   gotify,
   hdhomerun,
   homebridge,
-  ipsonder,
   jackett,
   jellyfin: emby,
   jellyseerr,
